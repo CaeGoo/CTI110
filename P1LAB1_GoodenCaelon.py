3f725e3c3dce9feed6334966e5057b3746dc619d
CaeGoo <goodencaelon@gmail.com>
2025-02-27 22:05:40 -0500

    Add files via upload

---
+++ P1LAB1_GoodenCaelon.py
@@ -0,0 +1,9 @@
+#CaelonGooden
+#02/28/2025
+#P1LAB1
+#Use inputs and outputs to create a program that allows user to enter information and then displays it back to them.
+print('Enter your first name:')
+first_name = input()
+print('Enter your last name:')
+last_name = input()
+print('Hello, ' + first_name + ' ' + last_name + ' Welcome to CTI-110!')
